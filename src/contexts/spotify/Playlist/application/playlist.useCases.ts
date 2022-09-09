19943a6bef7e981b9ad006e1aeafdbdf39f026fb
vVanish <chiriruho@gmail.com>
2022-09-09 01:05:58 -0400

    add: playlist creation from seed use case

--- src/contexts/spotify/Playlist/application/playlist.useCases.ts
+++ src/contexts/spotify/Playlist/application/playlist.useCases.ts
@@ -1,5 +1,9 @@
 import { deleteRequest, getRequest, postRequest } from "../../shared/request"
 import { getURLFromPathname, safeJoin } from "../../shared/spotify";
+import { getRecommendations } from "../../Track/application/track.useCases";
+import { SeedEntity } from "../../Track/model/seed.entity";
+import { getUserData } from "../../User/application/user.useCases";
+import { UserEntity } from "../../User/model/user.entity";
 import { PlaylistCreationOptions, PlaylistEntity, PlaylistItemCreationOptions } from "../model/playlist.entity";
 
 const PLAYLISTS_PATHNAME = "/playlists/";
@@ -16,21 +20,33 @@ const USER_PLAYLIST_CREATION_URL = (id: string) => safeJoin(USER_URL, id, PLAYLI
 const PLAYLIST_FOLLOWERS_URL = (id: string) => safeJoin(PLAYLISTS_API_URL, id, PLAYLISTS_FOLLOWERS_PATHNAME);
 
 // TODO add "additional_types", (filter) "fields" and "market" queries support
-const getPlaylist = (id: string, token: string) => {
+export const getPlaylist = (id: string, token: string) => {
 
   return getRequest<PlaylistEntity>(PLAYLIST_URL(id), { token });
 }
-
-const createPlaylist = (user_id: string, token: string, options: PlaylistCreationOptions) => {
+export const createPlaylist = (user_id: string, token: string, options: PlaylistCreationOptions) => {
 
   return postRequest<PlaylistEntity>(USER_PLAYLIST_CREATION_URL(user_id), options, { token });
 }
-const addTrackToPlaylist = (playlist_id: string, token: string, options: PlaylistItemCreationOptions) => {
+export const addTracksToPlaylist = (playlist_id: string, token: string, options: PlaylistItemCreationOptions) => {
 
   return postRequest<{ snapshot_id: string }>(PLAYLISTS_TRACKS_URL(playlist_id), options, { token });
 }
-const unfollowPlaylist = (playlist_id: string, token: string) => {
+export const unfollowPlaylist = (playlist_id: string, token: string) => {
   // This delete request doesnt return anything in particular, so i return true to signify that the unfollow was successful
   return deleteRequest(PLAYLIST_FOLLOWERS_URL(playlist_id), { token })
     .then(_ => true);
+}
+const DEFAULT_PLAYLIST_NAME = "Your Playlist";
+export const createRandomPlaylist = async (user: UserEntity, seed: SeedEntity, token: string, name?: string) => {
+
+  const tracks = await getRecommendations(seed, token);
+  const track_uris = tracks.map(track => track.uri);
+  console.log("Track URIs:", track_uris);
+  console.log("Track names:", tracks.map(track => track.name));
+
+  const playlist = await createPlaylist(user.id, token, { name: name ? name : DEFAULT_PLAYLIST_NAME });
+  await addTracksToPlaylist(playlist.id, token, { uris: track_uris });
+
+  return playlist.id;
 }
