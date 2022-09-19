387bce941e0c2c93d0e38c008c5a7b21c77d68e9
vVanish <chiriruho@gmail.com>
2022-09-19 18:53:36 -0400

    remove: app secret leak in commented code...

--- src/contexts/spotify/User/application/user.useCases.ts
+++ src/contexts/spotify/User/application/user.useCases.ts
@@ -71,14 +71,3 @@ export const getNewToken = async ({ app_id, app_secret, refresh_token }: TokenRe
   const response = await postRequest<RefreshedToken>(TOKEN_URL, new URLSearchParams(form), options)
   return response;
 }
-
-// console.log(getAuthorizationURL({ app_id: "1ae88614f2e34afdace9b8adb7239bdc", app_scope: "user-read-recently-played user-follow-read user-follow-modify playlist-read-private playlist-modify-public playlist-read-collaborative user-read-email user-read-private user-library-modify user-library-read", authorization_redirect: "https://www.iwi.com", state: "uwu" }));
-// const options: TokenCreationOptions = {
-//   code: "AQDxtQbG4PNLhBNYpmK6m_DS_ReV4P3Ei6Q-_YQsmiLcZGk8b2fpnZf2CNoW-nCDwKt2CsYUaxzRW_bVGbm4Ldsbev4pjh-AuVzyZjcVfWH5hkaFC6cX946dVXzQrzjCETaaambDQEz1rbkyfl9IbMqK3xGosY5qw0kgjr9zXlg7SqORZLHKGH-F6J0EuD53AcXXgHi1JlAySE7WNsTZL9du7kvTZRh7p4Psk8c_H54ji320qCvWZiJQBuZDQD-XfC-B9kpqPTzsKHckqyhAsWHPBf8dFsOzLOm0MWgYmAeFKVB2_hP-TPOyugJbVm0_IUrCUX9ydGqmJr1D_RMevwP9o5U7JL_y9zWKhzsnhHHrLIC-laiA28jeVfQ1nPbvVq9rdMQVJQZyPaZIgvmki7jdBl_uJ12nrJFx5FJBuYOszkG4Rtu7pQTvHV-QitBW",
-//   app_id: "1ae88614f2e34afdace9b8adb7239bdc",
-//   app_secret: "f90fc84350774709853c6d12425b6b4b",
-//   authorization_redirect: "https://www.iwi.com",
-// }
-// getTokenFromCode(options)
-//   .then(console.log)
-//   .catch(console.log);
